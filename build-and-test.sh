7fe00e421a1589575d434d59d204d53bcbe864bc
Robert O'Callahan <robert@ocallahan.org>
2016-06-23 20:28:05 +1200

    Update

--- build-and-test.sh
+++ build-and-test.sh
@@ -11,4 +11,4 @@ make -j8 -C ~/rr/obj
 make -j8 -C ~/rr/obj check
 rm -rf ~/.local/share/rr/*
 (rm -rf ~/tmp/firefox-profile ; mkdir ~/tmp/firefox-profile ; ~/rr/rr-vm-testing/xvnc-runner.sh "firefox --profile $HOME/tmp/firefox-profile $HOME/rr/rr-vm-testing/test.html" "rr Test Page")
-(rm -rf ~/.config/libreoffice ; ~/rr/rr-vm-testing/xvnc-runner.sh "libreoffice $HOME/rr/rr-vm-testing/rr-test-doc.odt" "rr-test-doc.odt")
+(rm -rf ~/.config/libreoffice ~/rr/rr-vm-testing/.~lock* ; ~/rr/rr-vm-testing/xvnc-runner.sh "libreoffice $HOME/rr/rr-vm-testing/rr-test-doc.odt" "rr-test-doc.odt")
